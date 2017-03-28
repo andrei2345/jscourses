6d970c26291b3a7ea2adf05468d2672ea8f85682
andrei2345 <andrei_234@tut.by>
2017-03-28 15:14:00 +0300

    Update zadanie2.3.js

--- zadanie2.3.js
+++ zadanie2.3.js
@@ -1,10 +1,10 @@
-var N = prompt ("Vvedite kol-vo elementov N");
-var X = prompt ("Vvedite X");
+var N =+ prompt ("Vvedite kol-vo elementov N");
+var X =+ prompt ("Vvedite X");
 var sum=0;
 var arr = new Array (N);
 
 for (var i = 0; i < arr.length; i++) {
-	N[i]=prompt("Vvedite znachenie a");
-	sum+=arr[i]*(Math.pow(X,N));
+	arr[i]=+prompt("Vvedite znachenie a");
+	sum+=arr[i]*(Math.pow(X,i));
 }
-alert(sum);
+alert("massiv: "+arr+ " summa: " +sum);
