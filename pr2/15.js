39e7697f3d005532c2bbaa550f32b0c675eeae14
andrei2345 <andrei_234@tut.by>
2017-05-26 06:31:21 +0300

    Update 15.js

--- pr2/15.js
+++ pr2/15.js
@@ -1,4 +1,6 @@
 var video = document.getElementsByTagName('video')[0];
+window.currentVideo=0;
+window.videosAmount =0;
 
 var play= document.getElementById('play')
 var stop = document.getElementById("stop")
@@ -12,170 +14,192 @@ var name = document.getElementById("video_name")
 var spisok= document.getElementById("spisok")
 var spis_com=document.getElementById("spis_com")
 
+
+
 function proigr (data,k,len){
 
- play.addEventListener("click",function() {
-  if (video.paused==false){
-    video.pause();
-    this.classList.toggle("pause")
-  }
-  else{
-    video.play()
-    this.classList.toggle("pause")
-    
-    }
-});
+      window.currentVideo = k;
+      console.log("Num: "+k);
 
- stop.addEventListener("click",function() {
-  video.pause();
-  video.currentTime = 0.0;
-  video.play()
-});
+   play.addEventListener("click",function() {
+          if (video.paused==false){
+            video.pause();
+            this.classList.toggle("pause")
+          }
+          else{
+            video.play()
+            this.classList.toggle("pause")
+            
+            }
+    });
 
- fwd.addEventListener("click",function() {
-  video.playbackRate=1*1.25;
-});
+   stop.addEventListener("click",function() {
+         
+          video.currentTime = 0.0;
+          video.play()
+    });
 
- rew.addEventListener("click",function(){
-  video.playbackRate= 1/1.25
-})
+   fwd.addEventListener("click",function() {
+          video.playbackRate=1*1.25;
+    });
 
- mut.addEventListener("click",function(){
-   if (video.muted==false){
-    video.muted=true
-    this.classList.toggle("mut")
-  } 
-  else {
-    this.classList.toggle("mut")
-    video.muted=false}
-  })
+   rew.addEventListener("click",function(){
+          video.playbackRate= 1/1.25
+    })
 
- video.volume=0.1
- volume.addEventListener('input',function(){
-  video.volume=volume.value/100
-})
-var curtime= document.getElementById("curtime");
- var time= document.getElementById("full")
+   mut.addEventListener("click",function(){
+           if (video.muted==false){
+            video.muted=true
+            this.classList.toggle("mut")
+          } 
+          else {
+            this.classList.toggle("mut")
+            video.muted=false}
+      })
 
- 
- video.addEventListener('loadedmetadata',function(){
-  var full = video.duration;
-  var x = Math.floor(full/60);
-  var sec = Math.floor(full%60)
-  var min= Math.floor(x%60);
-  console.log(min,sec)
-  var sec1= (":"+sec)
-  var min1= (min)
-  time.innerHTML=" / "+ min+sec1;
+   video.volume=0.1
+   volume.addEventListener('input',function(){
+          video.volume=volume.value/100
+    })
+    var curtime= document.getElementById("curtime");
+     var time= document.getElementById("full")
 
-  interval = setInterval(function(){
-    var full = video.currentTime;
-    var x = Math.floor(full/60);
-    var sec = Math.floor(full%60)
-    var min= Math.floor(x%60);
-    var sec1= (":"+sec)
-    var min1= (min)
-    curtime.innerHTML= min+sec1;
-  },1000)
-})
+     
+   video.addEventListener('loadedmetadata',function(){
+          var full = video.duration;
+          var x = Math.floor(full/60);
+          var sec = Math.floor(full%60)
+          var min= Math.floor(x%60);
+          // console.log(min,sec)
+          var sec1= (":"+sec)
+          var min1= (min)
+          time.innerHTML=" / "+ min+sec1;
 
-
-
-
- video.addEventListener("play",function(){
-  var progress= document.getElementById("progress")
-  var interval = setInterval(function(){
-    var time = video.currentTime*100/video.duration;
-    progress.style.width = time *3+'px';
-  }, 1000);
-})
-
- var player = document.getElementById("player")
- var prog = document.getElementById("progressBar")
- var progress = document.getElementById('progress')
-
- prog.addEventListener("click",function(e){
-   var widthProgressBar = progressBar.clientWidth;
-   var left =progressBar.getBoundingClientRect().left;
-   var percent = (e.clientX-left )/widthProgressBar;
-   video.currentTime = video.duration * percent;
- })
+          interval = setInterval(function(){
+            var full = video.currentTime;
+            var x = Math.floor(full/60);
+            var sec = Math.floor(full%60)
+            var min= Math.floor(x%60);
+            var sec1= (":"+sec)
+            var min1= (min)
+            curtime.innerHTML= min+sec1;
+          },1000)
+    })
 
 
-video.addEventListener("ended", function(){
- 
-    if(k == len-1){
-      video.setAttribute("src",data[k].address);
-      proigr(data, 0, len);
-    }
-    else {
-      video.setAttribute("src",data[k + 1].address);
-      proigr(data, k+1, len);
-    }
-  })
+
+
+    video.addEventListener("play",function(){
+          var progress= document.getElementById("progress")
+          var interval = setInterval(function(){
+            var time = video.currentTime*100/video.duration;
+            progress.style.width = time *3+'px';
+          }, 1000);
+    })
+
+     var player = document.getElementById("player")
+     var prog = document.getElementById("progressBar")
+     var progress = document.getElementById('progress')
+
+   prog.addEventListener("click",function(e){
+           var widthProgressBar = progressBar.clientWidth;
+           var left =progressBar.getBoundingClientRect().left;
+           var percent = (e.clientX-left )/widthProgressBar;
+           video.currentTime = video.duration * percent;
+     })
+
+
+    video.play();
+
+   video.addEventListener("ended", function(){
+        k = currentVideo;
+        len= videosAmount;
+
+        if (k!== len-1){
+           video.setAttribute("src",data[k + 1].address);
+          proigr(data, k+1, len);
+        }
+        else {
+         video.setAttribute("src",data[k].address);
+          proigr(data, 0, len);
+        }
+      })
 }
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", 'video.json', true);
 
 var  data = [];
-var sum=0; var reyting=0
+
 xhr.onload = function(){
   data=JSON.parse(xhr.responseText);
-console.log(xhr.responseText);
+
+console.log(data);
+    videosAmount = data.length;
  
 
  for (var i = 0; i < data.length; i++) {
-    
 
-    //   for ( var n=0; n< com.length; n++){
-    //  var li = document.createElement("li")
-
-
-
-    // spis_com.appendChild(li)
     var len = data.length;
     var li = document.createElement("li")
     var h3 = document.createElement("h3");
     var p = document.createElement("p");
+    var p1 = document.createElement("p1");
     spisok.appendChild(li);
     li.innerHTML= "<img src =\"" + data[i].poster + "\"> ";
     h3.innerHTML= data[i].name;
     li.appendChild(h3)
+
+
+    var sum=0; 
+    var com = data[i].comments;
+
+    for (var n = 0; n < com.length; n++) {
+               var mark=com[n].marks 
+                sum+=  mark;
+    } 
+      var rating = sum/com.length;
+    console.log("Rating: "+rating); 
+    p.innerHTML= "Рейтинг:"+rating;
+    li.appendChild(p)
+    
+
     var btn = document.createElement("button");
     li.appendChild(btn);
     li.setAttribute("value", i);
-    li.style.listStyle= "none"
+    li.style.listStyle= "none";
     btn.innerHTML = "►";
     btn.setAttribute("type", "button")
 
+                       
 
     btn.addEventListener("click",function(){
-     // spis_com.remoweChild(li);
-
-       proigr(data,k,len); 
-       var k = this.parentNode.value;
-      console.log(k)
-      video.setAttribute("src",data[k].address)
-      video.play()
-      
-    var com = data[k].comments
-   
-     for (var n = 0; n < com.length; n++) {
-           var mark=com[n].marks 
-      sum+=  mark
-     // reyting=sum/com.length
-      console.log(sum)
-       var li = document.createElement("li")
-       li.innerHTML= "Отзыв:"+com[n].commtext+ "Оценка:"+com[n].marks;
      
-       spis_com.appendChild(li)
-         console.log(com)
+             var k = this.parentNode.value;
+              
+            video.setAttribute("src",data[k].address)
+            // video.play()
+            proigr(data,k,len);
 
-     }
+            spis_com.innerHTML=" ";
+             var sum=0; 
+            var com = data[k].comments;
+           for (var n = 0; n < com.length; n++) {
+                        var mark=com[n].marks 
+                   sum+=  mark;
+                   var li = document.createElement("li")
+                    li.innerText= " ";
+                   li.innerText+= "Комментарий :"+com[n].commtext + " Оценка:"+com[n].marks;
+                   spis_com.appendChild(li) 
+                   
+            }
+               var  rating = sum/com.length;
+           console.log("Rating: "+ rating)
+            p1.innerHTML= "Рейтинг:"+rating;
+            li.appendChild(p1)
 
     })
-
+ 
   }
 }
 xhr.send()
